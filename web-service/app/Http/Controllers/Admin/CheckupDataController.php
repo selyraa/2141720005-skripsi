4318d12bc7dff6f48103cedeb6d51c97492499ef
selyraa <selyra.123@gmail.com>
2025-05-19 23:21:35 +0700

    fix: change success message to error on checkup deletion

--- web-service/app/Http/Controllers/Admin/CheckupDataController.php
+++ web-service/app/Http/Controllers/Admin/CheckupDataController.php
@@ -241,6 +241,6 @@ class CheckupDataController extends Controller
         $checkup->delete();
         
         return redirect()->route('checkups.index')
-            ->with('success', __('app.checkup_deleted_successfully'));
+            ->with('error', __('app.checkup_deleted_successfully'));
     }
 }
